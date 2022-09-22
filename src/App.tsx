d09b14e7debcf685fd911deddd00a6925c85266d
sangyouh <sangyouh@usc.edu>
2022-09-22 17:49:30 +0900

    working on scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { Environment, OrbitControls, useGLTF } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Suspense } from "react";
 
-function App() {
+const Model = () => {
+  let gltf = useLoader(GLTFLoader, "output.gltf");
+  const wut = useGLTF("output.gltf");
+  // let _ = require("lodash");
+  // const nodes = wut?.nodes;
+  const [nodes, setNodes] = React.useState({});
+  const gltfArray = Object.entries(gltf);
+  // const tt = _.groupBy(gltf?.nodes, "Mesh");
+
+  const temp = gltf.scene.children[0].children[0].children.filter((item) => {
+    return item.type === "LineSegments";
+  });
+
+  const temp2 = gltf.scene.children[0].children[0].children.filter((item) => {
+    return item.type === "Mesh";
+  });
+
+  gltf?.scene?.children[0].children[0].children?.map((item) => {
+    if (item.type === "Mesh" && item.id < 2000) {
+      // @ts-ignore
+      // item.material.color.isColor = false;
+      item.visible = false;
+      // item.material.opacity = 0;
+      // console.log(item);
+    }
+    // console.log("item", item.material);
+    // if (item.type === "Mesh") {
+    //   return (item.material.opacity = 0);
+    // }
+  });
+
+  // gltf.scene.children[0].children[0].children = temp2;
+
+  React.useEffect(() => {
+    setNodes(gltf?.nodes);
+    // setNodes(gltf?.nodes);
+  }, []);
+
+  console.log("GG", gltf);
+
+  // console.log("wit", temp);
+
+  return (
+    <>
+      <primitive object={gltf.scene} scale={0.55} position={[0, -30, 0]} />
+    </>
+  );
+};
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Model />
+          <OrbitControls />
+          <Environment preset="park" background />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
-
-export default App;
